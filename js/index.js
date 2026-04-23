7b7d9d9638b79c25fba42fd8507baa0c536b0b67
dianamorimoto <dianamorimoto89@gmail.com>
2026-04-23 01:40:22 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,3 +8,31 @@ document.querySelectorAll(".nav-links a").forEach(link => {
         document.getElementById("navlinks").classList.remove("active");
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let slideIndex = 0;
+    showSlide(slideIndex);
+
+    function showSlide(n) {
+        const slides = document.querySelectorAll(".campaign-slide");
+
+        if (n >= slides.length) slideIndex = 0;
+        if (n < 0) slideIndex = slides.length - 1;
+
+        slides.forEach(slide => slide.style.display = "none");
+        slides[slideIndex].style.display = "block";
+    }
+
+    document.querySelector(".next").addEventListener("click", () => {
+        slideIndex++;
+        showSlide(slideIndex);
+    });
+
+    document.querySelector(".prev").addEventListener("click", () => {
+        slideIndex--;
+        showSlide(slideIndex);
+    });
+    
+});
